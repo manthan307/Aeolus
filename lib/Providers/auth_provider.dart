072f31141bb89ea0dc1f2d7b6bb7351976c67b71
Manthan <pmanthan549@gmail.com>
2024-10-30 15:55:14 +0530

    restaring all

--- lib/Providers/auth_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final authProvider = Provider((ref) => {});
-
-// class auth extends StateNotifier<User>{
-//   Future<void> login() async{}
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,4 @@
-import 'package:aeolus/Screens/home.dart';
-import 'package:aeolus/Screens/login.dart';
-import 'package:aeolus/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-final _routes = GoRouter(routes: [
-  GoRoute(
-    path: "/",
-    builder: (context, state) => const Home(),
-    // redirect: (BuildContext context, GoRouterState state) async {
-    //   if (true) {
-    //     return '/login';
-    //   }
-    // },
-  ),
-  GoRoute(
-    path: "/login",
-    builder: (context, state) => const LoginPage(),
-  )
-]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -34,13 +6,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Aeolus',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
-      routerConfig: _routes,
+      home: const Scaffold(
+        body: Center(
+          child: Text("hello"),
+        ),
+      ),
     );
   }
 }
